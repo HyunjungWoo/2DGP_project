685ae8bf0c63746c06968e94b50e48b124eb10a5
HyunjungWoo <hjhs0628@naver.com>
2022-10-13 03:01:36 +0900

    보스몬스터 이미지 구현 완료
    10/13일 오전 3시까지

--- character.py
+++ character.py
@@ -2,42 +2,89 @@ from pico2d import *
 
 class CupHead_Character:
     def __init__(self):
-        self.x,self.y = 0,90
-        self.stay_frame = 0
-        self.run_frame = 0
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.sin = 16
         self.image2 = load_image('run_right.png')
         self.image1 = load_image('stay.png')
+
     def update(self):
-        self.run_frame = (self.run_frame + 1 ) % 16
-        self.stay_frame = (self.stay_frame + 1 ) % 5
+        self.frame = (self.frame + 1) % self.sin  # 달리기
+        # self.frame = (self.frame + 1 ) % scene #서있기 scene =5
         self.x += 5
 
-    def stay_draw(self):
-        self.image1.clip_draw(self.stay_frame * 169,330,169,225,self.x,self.y)## 330 간격으로 bottom 잡기
+    def draw(self):
+        #self.image1.clip_draw(self.frame * 169, 330, 169, 225, self.x, self.y)  ## 330 간격으로 bottom 잡기 stay 이미지
+        self.image2.clip_draw(self.frame * 190, 0, 190, 239, self.x, self.y)  ## 달리기 오른쪽으로 #달리기 이미지
+class BackGround:
+    def __init__(self):
+        self.background_image = load_image('background.png')
 
-    def run_draw(self):
-        self.image2.clip_draw(self.run_frame * 190,0,190,239,self.x,self.y)  ## 달리기 오른쪽으로
+    def draw(self):
+        self.background_image.draw(600,450,1200,900)
+
+class Potato_Monster:
+    def __init__(self):
+        self.frame = 0
+        self.sin = 5
+        self.image = load_image('potato.png')
+        self.ddong = load_image('ddong.png')
+        self.attack = load_image('potato_attack.png')
+        self.print = 0
+        self.x,self.y = 750,300
+    def update(self):
+        self.frame = (self.frame + 1) % self.sin
+    def draw(self):
+        #self.appear_motion()
+
+        self.attack_draw()
+        if(self.print>=12):
+            self.ddong_draw()
+    def appear_motion(self):
+        pass
+    def ddong_draw(self):
+
+        self.x -= 100
+        self.ddong.clip_draw(self.frame * 134, 1,133,138,self.x,100)
+    def attack_draw(self):
+        self.sin = 12
+        self.attack.clip_draw(self.frame * 542, 0, 542, 590, 880, self.y)
+        self.print += 1
 
 
 
 
-open_canvas()
+
+
+
+
+open_canvas(1200,900)
+background = BackGround()
+
+potato_monster = Potato_Monster()
 cuphead = CupHead_Character()
-running = True
+done = False
 
-while running:
+def runGame():
+    global done
 
-    #handle_events()
+    while not done:
+        #handle_events()
+        clear_canvas()
+        cuphead.update()
+        background.draw()
+        potato_monster.update()
 
-    cuphead.update()
+        potato_monster.draw()
 
-    clear_canvas()
-    #cuphead.stay_draw()
-    cuphead.run_draw()
-    update_canvas()
-
-    delay(0.25)
+        cuphead.draw()
 
 
 
-close_canvas()
+        update_canvas()
+
+        delay(0.25)
+
+
+runGame()
+# close_canvas()
